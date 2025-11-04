a9d991cd885cafd4b8bb7984b0d8a5d030bb9fd8
234354656 <nikolina.cavlovic10@gmail.com>
2025-11-04 09:23:26 +0100

    Spawn

---
+++ 3GJ-Phone/Assets/_scripts/EnemyMovement.cs
@@ -0,0 +1,27 @@
+using System;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public Transform playerTarget;
+    [SerializeField] public float movementSpeed = 10f;
+    [SerializeField] private float damage = 20f;
+    
+    private void Update()
+    {
+        transform.position = Vector3.MoveTowards(
+            transform.position,
+            playerTarget.position,
+            movementSpeed * Time.deltaTime);
+        transform.LookAt(playerTarget.position);
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.TryGetComponent(out PlayerHealth playerHealth))
+        {
+            playerHealth.TakeDamage(damage);
+            Destroy(gameObject);
+        }
+    }
+}
