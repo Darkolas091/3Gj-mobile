48c73ce5fe14f418723a46d6e27f9434ddc57e1d
ivana-antunovic <ivana.antunovic.ri@gmail.com>
2025-11-07 07:19:50 +0100

    dorada scene

--- 3GJ-Phone/Assets/Scripts/Player/CharacterControl.cs
+++ 3GJ-Phone/Assets/Scripts/Player/CharacterControl.cs
@@ -4,22 +4,25 @@ using UnityEngine.InputSystem;
 public class CharacterControl : MonoBehaviour
 {
     [SerializeField] private InputActionReference moveActionToUse;
-    [SerializeField] private float speed;
-
-    
-
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private Animator animator; 
 
     void Start()
     {
         
+        if (animator == null)
+            animator = GetComponentInChildren<Animator>();
     }
 
-   
     void Update()
     {
         Vector2 moveDirection = moveActionToUse.action.ReadValue<Vector2>();
         Vector3 move = new Vector3(moveDirection.x, 0f, moveDirection.y);
         move = transform.TransformDirection(move);
-        transform.Translate(move * speed * Time.deltaTime);
+
+        transform.Translate(move * speed * Time.deltaTime, Space.World);
+
+        float moveAmount = moveDirection.magnitude;
+        animator.SetFloat("Speed", moveAmount);
     }
 }
